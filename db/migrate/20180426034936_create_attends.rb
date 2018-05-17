f9bb0324c119acb9b2b26defdafbf2ab4965fe76
Nri <greatdmk@gmail.com>
2018-05-17 03:03:13 +0000

    modify migration files

--- db/migrate/20180426034936_create_attends.rb
+++ db/migrate/20180426034936_create_attends.rb
@@ -3,7 +3,7 @@ class CreateAttends < ActiveRecord::Migration[5.1]
     create_table :attends do |t|
       t.string :status
       t.string :secret_url
-      t.references :guest_user, foreign_key: true
+      # t.references :guest_user, foreign_key: true
       t.references :event, foreign_key: true
 
       t.timestamps
--- db/migrate/20180512191059_remove_guest_user_from_attend.rb
+++ db/migrate/20180512191059_remove_guest_user_from_attend.rb
@@ -1,6 +1,6 @@
 class RemoveGuestUserFromAttend < ActiveRecord::Migration[5.1]
   def change
-    remove_foreign_key :attends, :guest_users
-    remove_reference :attends, :guest_user, foreign_key: true
+    # remove_foreign_key :attends, :guest_users
+    # remove_reference :attends, :guest_user, foreign_key: true
   end
 end
