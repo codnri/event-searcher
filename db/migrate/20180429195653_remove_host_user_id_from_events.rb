cdaafe93b52e3288e48a666fdf1865d961679c51
Nri <greatdmk@gmail.com>
2018-05-17 03:08:39 +0000

    modify migration host_user

--- db/migrate/20180429195653_remove_host_user_id_from_events.rb
+++ db/migrate/20180429195653_remove_host_user_id_from_events.rb
@@ -1,5 +1,5 @@
 class RemoveHostUserIdFromEvents < ActiveRecord::Migration[5.1]
   def change
-    remove_reference :events, :host_user, index: true
+    # remove_reference :events, :host_user, index: true
   end
 end
